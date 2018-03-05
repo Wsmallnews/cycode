fb704a5f77295cebea5f0e9223caaa9a93a7af16
wangshengnan <806300238@qq.com>
2018-03-05 14:37:46 +0800

    cheyu

--- pages/nearby/nearby.js
+++ pages/nearby/nearby.js
@@ -6,25 +6,24 @@ Page({
   onLoad: function (options) {
     // 生命周期函数--监听页面加载
     showView: (options.showView == "true" ? true : false)
-  }
-  ,
+  },
   onLoad: function (options) {
     // 生命周期函数--监听页面加载
       showfilternav: (options.showfilternav == "true" ? true : false)
-  }
-  , onChangeShowState: function () {
+  },
+  onChangeShowState: function () {
     var that = this;
     that.setData({
       showView: (!that.data.showView)
     })
-  }
-  , onChangeShowSta: function () {
+  },
+  onChangeShowSta: function () {
     var that = this;
     that.setData({
       showfilternav: (!that.data.showfilternav)
     })
-  }
-  , onTap: function (event) {
+  },
+  onTap: function (event) {
     wx.navigateTo({
       url: "../seach/seach"
     });
